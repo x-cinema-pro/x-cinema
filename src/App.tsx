5a2d4ece48aced768966312255a35b48388c9dde
user <user@example.com>
2025-07-16 20:19:01 +0630

    feat: Implement lazy loading for routes to improve performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
 import Layout from './components/Layout';
-import Home from './pages/Home';
-import Movies from './pages/Movies';
-import TVShows from './pages/TVShows';
-import Genres from './pages/Genres';
-import Watch from './pages/Watch';
-import Details from './pages/Details';
 import './index.css';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Movies = React.lazy(() => import('./pages/Movies'));
+const TVShows = React.lazy(() => import('./pages/TVShows'));
+const Genres = React.lazy(() => import('./pages/Genres'));
+const Watch = React.lazy(() => import('./pages/Watch'));
+const Details = React.lazy(() => import('./pages/Details'));
 
 const queryClient = new QueryClient();
 
@@ -17,16 +19,18 @@ function App() {
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="movies" element={<Movies />} />
-              <Route path="tv" element={<TVShows />} />
-              <Route path="genres" element={<Genres />} />
-              <Route path="details/:type/:id" element={<Details />} />
-            </Route>
-            <Route path="/watch/:type/:id" element={<Watch />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="movies" element={<Movies />} />
+                <Route path="tv" element={<TVShows />} />
+                <Route path="genres" element={<Genres />} />
+                <Route path="details/:type/:id" element={<Details />} />
+              </Route>
+              <Route path="/watch/:type/:id" element={<Watch />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </QueryClientProvider>
     </HelmetProvider>
